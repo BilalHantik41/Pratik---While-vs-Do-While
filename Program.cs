da3c7262c22ee5bb296a5fd1206da25ccd7f082d
Bilal Hantik <bilalhantik@gmail.com>
2025-05-08 19:19:41 +0300

    While ve Do While Pratik
    While ve Do While Pratik

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WhileDoWhile
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {/*
+            Console.WriteLine("1. Soru: While Döngüsü");
+            Console.WriteLine("---------------------------------------------");
+           int Sayac = 0;
+           int Limit = 100;  // Burdaki Değer Kadar dönmeden sistem kendini tamamlamıcak.
+            while (Sayac <= Limit )
+            {
+                Console.WriteLine(Sayac +" kez"+" "+"Ben bir Patika'liyim");
+                Sayac++;
+            }
+            Console.WriteLine("---------------------------------------------");
+
+            */
+
+            Console.WriteLine("2. Soru: Do While Döngüsü");
+            Console.WriteLine("---------------------------------------------");
+            int Sayac = 1;
+            int Limit = 100; // Burayi 0 yapsan bile Konsola 1 kez Ben bir patikaliyim yazicak ve döngüyü bitiricek
+            do
+            {
+               Console.WriteLine(Sayac +" kez"+" "+"Ben bir Patika'liyim");
+                Sayac++;
+            } 
+            
+            while (Sayac <= Limit);
+            {   
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Döngü Bitti ve İşlem Sonlandi...");
+            }
+            Console.WriteLine("---------------------------------------------");
+        }    // Yorum Satirlarini kaldirarak While Döngüsünü ayri Do While Döngüsünü ayri çalıştırmalisiniz. 
+             //İkisi aynanda çalışırsa 100 kez While döngüsünde yazar 1 kezde Do While döngüsünde 101 kez yazmış olur
+
+             // Do While Şeklinde bir kod bloğu en az 1 kez çalışmak zorundadir. Limit 0 yapılsa bile 1 kez çalıştiktan sonra sonlanir.
+             // While döngüsünde limit kadar çalışmadan sistem durmaz.
+    }
+}
